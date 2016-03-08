d3cf69bc57ec298c49ac4e49cbda55560ccbe1f4
Daniel Cai <daniel.cai@pwc.com>
2016-03-08 16:57:39 +0800

    learning

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDom from 'react-dom';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "AIzaSyBrEwgQJYJ_pybBclMDlf9xXFmqE0t6BLo";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+	return <div>Hi!</div>
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDom.render(<App/>,document.querySelector('.container'));
